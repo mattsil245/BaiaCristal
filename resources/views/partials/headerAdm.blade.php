bd1173aea5fb7ae06164bc2b6311aa07a368ff91
mattsil245 <moliveiradasilva494@gmail.com>
2025-06-02 09:47:45 -0300

    com sair

--- resources/views/partials/headerAdm.blade.php
+++ resources/views/partials/headerAdm.blade.php
@@ -41,14 +41,19 @@
     <div class="dropdown">
       <a href="#" class="d-flex align-items-center link-dark text-decoration-none dropdown-toggle" id="dropdownUser2" data-bs-toggle="dropdown" aria-expanded="false">
         <img src="https://github.com/mdo.png" alt="" width="32" height="32" class="rounded-circle me-2">
-        <strong>mdo</strong>
+        <strong>{{ session('admin')->nome ?? 'Admin' }}</strong>
       </a>
       <ul class="dropdown-menu text-small shadow" aria-labelledby="dropdownUser2">
         <li><a class="dropdown-item" href="#">New project...</a></li>
         <li><a class="dropdown-item" href="#">Settings</a></li>
         <li><a class="dropdown-item" href="#">Profile</a></li>
         <li><hr class="dropdown-divider"></li>
-        <li><a class="dropdown-item" href="#">Sign out</a></li>
+<li>
+    <form method="POST" action="{{ route('admin.logout') }}">
+        @csrf
+        <button type="submit" class="dropdown-item">Sair</button>
+    </form>
+</li>
       </ul>
     </div>
   </div>
